219ed25a9b42298327151c05e6d3f00430a4dd56
Murilo Zanin <murilo.sousa@norus.com.br>
2022-12-12 09:32:40 -0300

    Junção com mapas ajustes

--- dist/js/Mapas/imagemMapa.js
+++ dist/js/Mapas/imagemMapa.js
@@ -5,39 +5,44 @@
 
     const d3 = window.d3;
 
-    const criarPNGMapa = function () {
+    const criarPNGMapa = function (idGerarImg, idDownloadPng, idDataSvg, idDataPng) {
         
-        // d3.select("#save").on("click", function(){
-        const html = d3.select("svg")
-            .attr("version", 1.1)
-            .attr("xmlns", "http://www.w3.org/2000/svg")
-            .node().parentNode.innerHTML;
-
-        const imgsrc = 'data:image/svg+xml;base64,'+ btoa(html);
-        const img = '<img src="'+imgsrc+'">'; 
-        d3.select("#svgdataurl").html(img);
-
-        const canvas = document.querySelector("canvas"),
-            context = canvas.getContext("2d");
-
-        const image = new Image;
-        image.src = imgsrc;
-        image.onload = function() {
-            context.drawImage(image, 0, 0);
-        
-            const canvasdata = canvas.toDataURL("image/png");
-        
-            const pngimg = '<img src="'+canvasdata+'">'; 
-            d3.select("#pngdataurl").html(pngimg);
-        
-            const a = document.createElement("a");
-            a.download = "mapa.png";
-            a.href = canvasdata;
-            a.click();
-        };
-
-        // });
-
+        d3.select(idGerarImg).on("click", function(){
+            const html = d3.select("svg")
+                  .attr("version", 1.1)
+                  .attr("xmlns", "http://www.w3.org/2000/svg")
+                  .node().parentNode.innerHTML;
+          
+            //console.log(html);
+            const imgSrc = 'data:image/svg+xml;base64,'+ btoa(html);
+            const img = '<img src="'+imgSrc+'">'; 
+            d3.select(idDataSvg).html(img);
+          
+          
+            const canvas = document.querySelector("canvas"),
+              context = canvas.getContext("2d");
+          
+            const image = new Image;
+            image.src = imgSrc;
+            image.onload = function() {
+              context.drawImage(image, 0, 0);
+            
+              const canvasData = canvas.toDataURL("image/png");
+            
+              const pngImg = '<img style="width: 450px" src="'+canvasData+'">'; 
+              d3.select(idDataPng).html(pngImg);
+              d3.select(idDownloadPng).on("click", function(){
+                downloadMapa(canvasData)});
+            };
+          
+          });
+          //Download PNG -------------------------------------------------------------------------
+          function downloadMapa(canvasData){
+              const a = document.createElement("a");
+              a.download = "mapa.png";
+              a.href = canvasData;
+              a.click();
+            };
     }
 
 
